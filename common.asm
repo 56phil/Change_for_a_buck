d781ff69a4f0e8dcfd38a5b985daa6fb2cacf6d9
Phil Huffman <56phil@gmail.com>
2022-06-09 12:37:17 -0500

    adds readSTDIN & readuint

--- common.asm
+++ common.asm
@@ -1,19 +1,112 @@
 .text
 .align 8
 
+atouint:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  converts an ASCII string to an unsigned int
+;;
+;;  register usage:
+;;    x28 workarea size located at bottom of stack frame
+;;    x27 misc. index
+;;    x26 converted number
+;;    x25 radix (10)
+;;    x24 current factor
+;;    x23 unused
+
+    stp     fp, lr, [sp, #-16]!     ; preserve
+    stp     x27, x28, [sp, #-16]!   ; preserve
+    stp     x25, x26, [sp, #-16]!   ; preserve
+    stp     x23, x24, [sp, #-16]!   ; preserve
+    mov     x28, #64                ; size of workarea keep it a multiple of 16
+    sub     sp, sp, x28             ; move stack pointer down n * 16 bytes, space for ASCII string
+    add     fp, sp, x28             ; account for work area size
+    add     fp, fp, #64             ; finish setting up frame pointer by adding size of save area
+
+    sub     x27, x28, #8            ; initialize index
+    mov     x23, xzr                ; to clear work area
+clr_loop:                           ; ensure input area is all low values
+    str     x23, [sp, x27]          ; set to low values
+    subs    x27, x27, #8            ; set index for next quad
+    b.ge    clr_loop                ; iterate
+
+    add     x1, sp, #1              ; start of workarea
+    sub     x2, x28, #1             ; size of workarea
+    bl      readSTDIN
+    cmp     x0, xzr                 ; check x0 (length)
+    b.le    atouint_exit            ; negative length???
+    mov     x25, #10                ; radix
+    mov     x24, #1                 ; current factor
+    mov     x26, xzr                ; set sum to zero
+    mov     x27, x0                 ; index of lsd
+acc_loop:
+    ldrb    w23, [sp, x27]          ; load ascii digit
+    cmp     w23, wzr                ; end of input string?
+    b.le    acc_exit                ; yep, leave loop
+    cmp     w23, #0x30              ; compare to "0"
+    b.lt    acc_next                ; skip, too low
+    cmp     w23, #0x39              ; compare to "9"
+    b.gt    acc_next                ; skip, too high
+    sub     w23, w23, #0x30         ; deasciify digit
+    madd    x26, x23, x24, x26      ; multiply by power of 10
+    mul     x24, x24, x25           ; next power of ten
+acc_next:
+    subs    x27, x27, #1            ; index of next char
+    b.gt    acc_loop                ; continue
+acc_exit:
+    mov     x0, x26                 ; time to convert & print
+    mov     x1, xzr                 ; no padding
+    bl      printUInt               ; to STDOUT
+    bl      EOL
+
+atouint_exit:
+    add     sp, sp, x28             ; return string work area
+    ldp     x23, x24, [sp], #16     ; restore
+    ldp     x25, x26, [sp], #16     ; restore
+    ldp     x27, x28, [sp], #16     ; restore
+    ldp     fp, lr, [sp], #16       ; restore
+    ret                             ; return
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 print:
     mov     x0, #1                  ; STDOUT
-    mov     x16, 4                  ; write
+    mov     x16, #4                 ; write
     svc     #0xffff
     ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+EOL:                                ; end of line routine
+;;
+;;  register usage:
+;;    x28 workarea size located at bottom of stack frame
+;;    x27 unused
+
+    stp     fp, lr, [sp, #-16]!     ; preserve
+    stp     x27, x28, [sp, #-16]!   ; preserve
+    mov     x28, #16                ; size of workarea keep it a multiple of 16
+    sub     sp, sp, x28             ; move stack pointer down n * 16 bytes, space for ASCII string
+    add     fp, sp, x28             ; account for work area size
+    add     fp, fp, #32             ; finish setting up frame pointer
+
+    mov     x1, #0xa                ; new line char
+    strb    w1, [sp, #1]            ; first byte of output gets it
+    add     x1, sp, #1              ; point to output area
+    mov     x2, #1                  ; print just the new line
+    bl      print
+
+    add     sp, sp, x28             ; return work area
+    ldp     x27, x28, [sp], #16     ; restore
+    ldp     fp, lr, [sp], #16       ; restore
+    ret                             ; return
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+printUInt:
 ;;  Print value in x0 as an unisgned int to STDOUT
 ;;  When x1 == 0 do not pad. Otherwise, right justify in a x1 byte field padded with spaces
+;;  x28 size of workarea
 ;;  x26 misc. index
-;;  x25 size of workarea
 ;;  x24 format indicator (size of field, or zero)
 ;;  x23 remainder
 ;;  x22 work register, quotient
@@ -21,29 +114,29 @@ print:
 ;;  x20 digit string index
 ;;  x19 radix/divisor
 ;;
-;;  inputs
+;;  inputs:
 ;;      x0 number to be printed
 ;;      x1 format indicator
-
-printUInt:
+;
     stp     fp, lr, [sp, #-16]!     ; preserve
+    stp     x27, x28, [sp, #-16]!   ; preserve
     stp     x25, x26, [sp, #-16]!   ; preserve
     stp     x23, x24, [sp, #-16]!   ; preserve
     stp     x21, x22, [sp, #-16]!   ; preserve
     stp     x19, x20, [sp, #-16]!   ; preserve
-    mov     x25, #32                ; size of workarea keep it a multiple of 16
-    sub     sp, sp, x25             ; move stack pointer down n * 16 bytes, space for digit string
-    add     fp, sp, x25             ; account for work area
-    add     fp, fp, #80             ; finish defining frame
-;
+    mov     x28, #32                ; size of workarea keep it a multiple of 16
+    sub     sp, sp, x28             ; move stack pointer down n * 16 bytes, space for digit string
+    add     fp, sp, x28             ; account for work area
+    add     fp, fp, #96             ; finish defining frame
+
     mov     x24, x1                 ; keep format indicator
     cmp     x24, xzr                ; padding requested?
     b.eq    normal                  ; nope, regular processing
     b.lt    invalid_fi              ; format indicator out of range
-    cmp     x24, x25                ; field size fits in workarea?
+    cmp     x24, x28                ; field size fits in workarea?
     b.gt    invalid_fi              ; nope, max padding
 clr_wrk:
-    mov     x26, x25                ; index (size of workarea)
+    mov     x26, x28                ; index (size of workarea)
     mov     x22, #0x20              ; put a " " in lsb
 init_loop:                          ; fill workarea with blanks
     strb    w22, [sp, x26]          ; insert another blank
@@ -70,23 +163,24 @@ printUInt_Count:
 printUInt_print:
     cmp     x24, xzr                ; pad?
     b.eq    nopad                   ; nope
-    mov     x2, x24                 ; size of field
+    sub     x2, x24, #1             ; size of field
 reverse_and_print:
     add     x1, sp, #1              ; point to output
     bl      reverse_field           ; undo algorithm
     bl      print
 
 printUInt_exit:
-    add     sp, sp, x25             ; return string work area
+    add     sp, sp, x28             ; return string work area
     ldp     x19, x20, [sp], #16     ; restore
     ldp     x21, x22, [sp], #16     ; restore
     ldp     x23, x24, [sp], #16     ; restore
     ldp     x25, x26, [sp], #16     ; restore
+    ldp     x27, x28, [sp], #16     ; restore
     ldp     fp, lr, [sp], #16       ; restore
     ret                             ; return
 
 invalid_fi:
-    mov     x24, x25                ; max padding
+    mov     x24, x28                ; max padding
     b       clr_wrk                 ; continue
 
 nopad:
@@ -101,14 +195,28 @@ printUInt_Zero:                     ; this is the exceptional case when x21 is 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+readSTDIN:
+;;  x0 returns string length
+;;  x1 buffer
+;;  x2 buffer size
+
+    stp     fp, lr, [sp, #-16]!     ; preserve
+    mov     x0, #0                  ; STDIN
+    mov     x16, #3                 ; read
+    svc     0xffff
+    ldp     fp, lr, [sp], #16       ; restore
+    ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 reverse_field:
-;;  swaps bytes so that "54321" becomes "12345" 
+;;  swaps bytes so that "54321" becomes "12345"
 ;;  x28 index of last unswapped byte
 ;;  x27 index of next byte to swap
 ;;  x26, x25 work registers
 ;; inputs
-;;  x1 size of field    const
-;;  x0 start of field   const
+;;  x2 size of field    const
+;;  x1 start of field   const
 
     cmp     x2, #1                  ; anything to swap?
     b.le    get_out                 ; no, get out
